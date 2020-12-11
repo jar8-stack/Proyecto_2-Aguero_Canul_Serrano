7398e59bc6fbc952f3e99a55af5ec4e6372fc249
jar8-stack <bioshock450226@gotmail.com>
2020-12-10 23:39:28 -0600

    Olo

--- app/src/main/java/com/example/proyecto_1_aguero_castillo_canul_serrano/db/Score_per_match_Dao.kt
+++ app/src/main/java/com/example/proyecto_1_aguero_castillo_canul_serrano/db/Score_per_match_Dao.kt
@@ -9,6 +9,9 @@ interface Score_per_match_Dao {
     @Query("SELECT * FROM score_per_match WHERE id_usuario=:idUser")
     fun getScorePerUser(idUser: Int): List<score_per_match>
 
+    @Query("SELECT * FROM score_per_match ORDER  BY score_per_match DESC LIMIT 0, 5")
+    fun getFiveScores(): List<score_per_match>
+
     @Insert
     fun insertScorePerMatch(SPM: score_per_match)
 }
