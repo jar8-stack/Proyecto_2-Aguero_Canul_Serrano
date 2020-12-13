41f509724798110af72f1368203d4882803709d1
ManuelCanul109 <manuel.canul109@gmail.com>
2020-12-12 19:33:18 -0500

    Arreglo de themesDAO

--- app/src/main/java/com/example/proyecto_1_aguero_castillo_canul_serrano/db/ConfigurationThemesDAO.kt
+++ app/src/main/java/com/example/proyecto_1_aguero_castillo_canul_serrano/db/ConfigurationThemesDAO.kt
@@ -6,7 +6,13 @@ import androidx.room.Query
 @Dao
 interface ConfigurationThemesDAO {
 
-    @Query("INSERT INTO configuration_themes(id_configuration_theme,id_configuration,id_theme) VALUES(null,:idConfiguration,:idTheme)")
-    fun insertConfigurationTheme(idConfiguration:Int,idTheme:Int)
+    @Query("INSERT INTO configuration_themes(id_configuration_theme,id_configuration,id_theme,status_configuration_themes) VALUES(null,:idConfiguration,:idTheme,:status)")
+    fun insertConfigurationTheme(idConfiguration:Int,idTheme:Int, status:Boolean)
+
+    @Query("SELECT status_configuration_themes FROM configuration_themes WHERE id_configuration = :idConfiguration AND id_theme = :idTema")
+    fun traerStatusTema(idConfiguration: Int, idTema: Int) : Boolean
+
+    @Query("UPDATE configuration_themes SET status_configuration_themes = :statusTema WHERE id_configuration = :idConfiguracion AND id_theme = :idTema")
+    fun actualizarStatusTema(idTema:Int,idConfiguracion: Int,statusTema:Boolean)
 
 }
