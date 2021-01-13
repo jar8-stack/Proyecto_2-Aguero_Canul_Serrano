26d8a3f619212f5d6e4ecefdf9acc44eca3738b6
jar8-stack <bioshock450226@gotmail.com>
2021-01-12 19:12:46 -0600

    Proyecto totalmente terminado

--- app/src/main/java/com/example/proyecto_1_aguero_castillo_canul_serrano/MemoramaActivity.kt
+++ app/src/main/java/com/example/proyecto_1_aguero_castillo_canul_serrano/MemoramaActivity.kt
@@ -2,10 +2,19 @@ package com.example.proyecto_1_aguero_castillo_canul_serrano
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MemoramaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_memorama)
+
+        //Hola mundo
+        val database = Firebase.database
+        val myRef = database.getReference("message")
+
+        myRef.setValue("Hello, World!")
+
     }
 }
