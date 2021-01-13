0825d4dbfa254fe0013fb4f6f6ab29f72479e7f5
jar8-stack <bioshock450226@gotmail.com>
2021-01-13 02:49:27 -0600

    muestra los usuarios que si quieren partidas y aparte te excluye a ti

--- app/src/main/java/com/example/proyecto_1_aguero_castillo_canul_serrano/MemoramaActivity.kt
+++ app/src/main/java/com/example/proyecto_1_aguero_castillo_canul_serrano/MemoramaActivity.kt
@@ -1,5 +1,6 @@
 package com.example.proyecto_1_aguero_castillo_canul_serrano
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ListView
@@ -15,14 +16,28 @@ class MemoramaActivity : AppCompatActivity() {
     lateinit var refPerUser: DatabaseReference
     lateinit var userList: MutableList<User_fire>
     lateinit var listView: ListView
+    lateinit var refBack: DatabaseReference
+
+    override fun onBackPressed() {
+        refBack = FirebaseDatabase.getInstance().getReference("Usuarios")
+        var userName= intent.getStringExtra("userName")
+
+        refBack.child(userName).child("buscando_partida").setValue("no")
+
+        val intent: Intent = Intent(this, SelectPlayerActivity::class.java)
+        startActivity(intent)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_memorama)
 
-        userList= mutableListOf()
+        userList = mutableListOf()
+        var userName= intent.getStringExtra("userName")
 
-        ref= FirebaseDatabase.getInstance().getReference("Usuarios")
+        ref = FirebaseDatabase.getInstance().getReference("Usuarios")
+
+
 
         listView= findViewById(R.id.listUsers)
 
@@ -30,12 +45,18 @@ class MemoramaActivity : AppCompatActivity() {
         ref.addValueEventListener(object: ValueEventListener{
             override fun onDataChange(snapshot: DataSnapshot) {
                 if(snapshot!!.exists()){
+                    userList.clear()
                     for(u in snapshot.children){
                         val user= u.getValue(User_fire::class.java)
                         if (user != null) {
                             user.userName= u.key.toString()
                         }
-                        userList.add(user!!)
+                        if (user != null) {
+                            if(user.buscando_partida=="si" && user.userName != userName){
+                                userList.add(user!!)
+                            }
+                        }
+
                     }
 
                     val adapter= User_adapter(applicationContext, R.layout.users, userList)
@@ -49,8 +70,8 @@ class MemoramaActivity : AppCompatActivity() {
 
         });
 
-        btnActivoPartida.setOnClickListener {
-            var userName= intent.getStringExtra("userName")
+
+
 
             //Hola mundo
             val database = Firebase.database
@@ -60,7 +81,8 @@ class MemoramaActivity : AppCompatActivity() {
             //myRef.setValue("Hello "+userName.toString())
             myRef.child("Usuarios").child(userName).child("buscando_partida").setValue("si")
             myRef.child("Usuarios").child(userName).child("puntaje_por_partida").setValue(0)
-        }
+
+
 
     }
 }
