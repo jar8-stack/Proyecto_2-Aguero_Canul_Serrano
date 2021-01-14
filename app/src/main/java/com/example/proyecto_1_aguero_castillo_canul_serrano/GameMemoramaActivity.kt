730d966179a22f2a2c4d6b947484864b21aff634
ManuelCanul109 <manuel.canul109@gmail.com>
2021-01-13 23:53:40 -0500

    Insert src ImageButton

--- app/src/main/java/com/example/proyecto_1_aguero_castillo_canul_serrano/GameMemoramaActivity.kt
+++ app/src/main/java/com/example/proyecto_1_aguero_castillo_canul_serrano/GameMemoramaActivity.kt
@@ -1,10 +1,14 @@
 package com.example.proyecto_1_aguero_castillo_canul_serrano
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_game_memorama.*
+
 
 class GameMemoramaActivity : AppCompatActivity() {
     lateinit var jugador1Text: TextView
@@ -24,12 +28,10 @@ class GameMemoramaActivity : AppCompatActivity() {
         jugador1Text.setText(jugador1)
         jugador2Text.setText(jugador2)
 
-        refInviEnvi.child(jugador2).child("invitaciones_enviadas").setValue("")
-        refInviEnvi.child(jugador1).child("invitaciones_recibidas").setValue("")
+        //refInviEnvi.child(jugador2).child("invitaciones_enviadas").setValue("")
+        //refInviEnvi.child(jugador1).child("invitaciones_recibidas").setValue("")
 
 
-
-
-
+        boton00.setBackgroundResource(R.drawable.fondo);
     }
 }
