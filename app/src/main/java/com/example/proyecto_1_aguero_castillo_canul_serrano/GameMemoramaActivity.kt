e98cddad87b468fe5a6123f65d7f5f31148165af
jar8-stack <bioshock450226@gotmail.com>
2021-01-13 21:36:52 -0600

    listo por fin

--- app/src/main/java/com/example/proyecto_1_aguero_castillo_canul_serrano/GameMemoramaActivity.kt
+++ app/src/main/java/com/example/proyecto_1_aguero_castillo_canul_serrano/GameMemoramaActivity.kt
@@ -2,22 +2,27 @@ package com.example.proyecto_1_aguero_castillo_canul_serrano
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.google.firebase.database.FirebaseDatabase
 
 class GameMemoramaActivity : AppCompatActivity() {
+    lateinit var jugador1Text: TextView
+    lateinit var jugador2Text: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_memorama)
 
-        /*
 
         var jugador1= intent.getStringExtra("jugador1")
         var jugador2= intent.getStringExtra("jugador2")
 
-        //sistma de confirmaciónes
-        val refConfir= FirebaseDatabase.getInstance().getReference("Usuarios")
-        refConfir.child(jugador2).child("invitaciones_enviadas").child(jugador1).child("confirma").setValue("si")
-        */
+        jugador1Text= findViewById(R.id.jugador1)
+        jugador2Text= findViewById(R.id.jugador2)
+
+        jugador1Text.setText(jugador1)
+        jugador2Text.setText(jugador2)
+
+
 
 
 
