a2eae9072e8588a9e49fceb43cdc53062ba0c0f4
jar8-stack <bioshock450226@gotmail.com>
2021-01-13 19:10:09 -0600

    notificaciones en proceso

--- app/src/main/java/com/example/proyecto_1_aguero_castillo_canul_serrano/MemoramaActivity.kt
+++ app/src/main/java/com/example/proyecto_1_aguero_castillo_canul_serrano/MemoramaActivity.kt
@@ -3,7 +3,11 @@ package com.example.proyecto_1_aguero_castillo_canul_serrano
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ListView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.proyecto_1_aguero_castillo_canul_serrano.prueba
 import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
@@ -17,6 +21,8 @@ class MemoramaActivity : AppCompatActivity() {
     lateinit var userList: MutableList<User_fire>
     lateinit var listView: ListView
     lateinit var refBack: DatabaseReference
+    lateinit var refInviEnvi: DatabaseReference
+
 
     override fun onBackPressed() {
         refBack = FirebaseDatabase.getInstance().getReference("Usuarios")
@@ -61,12 +67,16 @@ class MemoramaActivity : AppCompatActivity() {
 
                     val adapter= User_adapter(applicationContext, R.layout.users, userList)
                     listView.adapter = adapter
+
+
                 }
             }
+
 
             override fun onCancelled(error: DatabaseError) {
                 TODO("Not yet implemented")
             }
+
 
         });
 
@@ -81,6 +91,54 @@ class MemoramaActivity : AppCompatActivity() {
             //myRef.setValue("Hello "+userName.toString())
             myRef.child("Usuarios").child(userName).child("buscando_partida").setValue("si")
             myRef.child("Usuarios").child(userName).child("puntaje_por_partida").setValue(0)
+
+        listView.onItemClickListener = object : AdapterView.OnItemClickListener{
+            override fun onItemClick(
+                parent: AdapterView<*>, view: View,
+                position: Int, id: Long
+            ) {
+
+                // value of item that is clicked
+                val itemValue = listView.getItemAtPosition(position) as User_fire
+
+                var userNameInvi= itemValue.userName
+
+
+                refInviEnvi = FirebaseDatabase.getInstance().getReference("Usuarios")
+
+                refInviEnvi.child(userNameInvi).child("invitaciones_recibidas").setValue(userName)
+                refInviEnvi.child(userName).child("invitaciones_enviadas").setValue(userNameInvi)
+
+
+                refInviEnvi.child(userName).child("invitaciones_recibidas").addValueEventListener(object: ValueEventListener{
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        val mAlertDialog= AlertDialog.Builder(this@MemoramaActivity)
+                        mAlertDialog.setTitle("Invitación de partida")
+                        mAlertDialog.setMessage(userNameInvi+" te ha invitado a una partida ¿Aceptas?")
+                        mAlertDialog.setPositiveButton("Si") GameMemoramaActivity@{ dialog, id ->
+
+
+                            return@GameMemoramaActivity
+                        }
+
+                        mAlertDialog.setNegativeButton("No"){ dialog, id ->
+                            refInviEnvi.child(userNameInvi).child("invitaciones_enviadas").setValue("")
+                            dialog.dismiss()
+                        }
+
+                        mAlertDialog.show()
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+                        TODO("Not yet implemented")
+                    }
+
+                })
+
+
+
+            }
+        }
 
 
 
--- app/src/main/java/com/example/proyecto_1_aguero_castillo_canul_serrano/User_fire.kt
+++ app/src/main/java/com/example/proyecto_1_aguero_castillo_canul_serrano/User_fire.kt
@@ -1,7 +1,7 @@
 package com.example.proyecto_1_aguero_castillo_canul_serrano
 
-data class User_fire(var userName: String, val buscando_partida: String, val puntaje_por_partida: Int) {
-    constructor() : this("", "", 0){
+data class User_fire(var userName: String, val buscando_partida: String, val puntaje_por_partida: Int, val invitaciones_recibidas: String, val invitaciones_enviadas: String) {
+    constructor() : this("", "", 0, "", ""){
 
     }
 }
