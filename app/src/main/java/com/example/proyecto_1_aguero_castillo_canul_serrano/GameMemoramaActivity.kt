65a43e6bf1766d7f4ca8206603246cc7b17b6201
Carlos Ivan Aguero <aguero_5209@hotmail.com>
2021-01-14 20:33:46 -0600

    2 in a match

--- app/src/main/java/com/example/proyecto_1_aguero_castillo_canul_serrano/GameMemoramaActivity.kt
+++ app/src/main/java/com/example/proyecto_1_aguero_castillo_canul_serrano/GameMemoramaActivity.kt
@@ -265,19 +265,19 @@ class GameMemoramaActivity : AppCompatActivity() {
             }
 
             override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
-                TODO("Not yet implemented")
+
             }
 
             override fun onChildRemoved(snapshot: DataSnapshot) {
-                TODO("Not yet implemented")
+
             }
 
             override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
-                TODO("Not yet implemented")
+
             }
 
             override fun onCancelled(error: DatabaseError) {
-                TODO("Not yet implemented")
+
             }
 
         })
@@ -290,19 +290,19 @@ class GameMemoramaActivity : AppCompatActivity() {
             }
 
             override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
-                TODO("Not yet implemented")
+
             }
 
             override fun onChildRemoved(snapshot: DataSnapshot) {
-                TODO("Not yet implemented")
+
             }
 
             override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
-                TODO("Not yet implemented")
+
             }
 
             override fun onCancelled(error: DatabaseError) {
-                TODO("Not yet implemented")
+
             }
 
         })
